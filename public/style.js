bbfd0462798994bea18fe0c2b5b3d4aff7b8fdd4
shadabmajeed <44368474+shadabmajeed@users.noreply.github.com>
2020-04-07 03:50:15 +0530

    Update style.js

--- public/style.js
+++ public/style.js
@@ -3,50 +3,40 @@ var country=$('#country');
 var btn=$('#btn');
 var tableh=$('#thead')
 var tableb=$('#tbody')
-$('#lastupdate').hide()
-btn.click(()=>{
+btn.click(()=>{})
     var conname=country.val()
     var val=conname.charAt(0).toUpperCase()+conname.substring(1).toLowerCase();
+    var mydata;
+    $.ajax({
+        url: 'https://api.thevirustracker.com/free-api?countryTotals=ALL',
+        dataType: 'json',
+        success: function(data) {
+          mydata=data;
+          tableh.children().remove()
+          tableb.children().remove()
    
-    var settings = {
-        "async": true,
-        "crossDomain": true,
-        "url": `https://covid-19-coronavirus-statistics.p.rapidapi.com/v1/stats?country=${val}`,
-        "method": "GET",
-        "headers": {
-            "x-rapidapi-host": "covid-19-coronavirus-statistics.p.rapidapi.com",
-            "x-rapidapi-key": "178daf1067msh9a869b74211b0b1p16b4f8jsna837fd521321"
+           tableh.attr({"class":"table-secondary"})
+           tableh.append($('<tr>').append($('<th>').text('country')).append($('<th>').text('totalcases')).append($('<th>').text('active cases')).append($('<th>').text('deaths')).append($('<th>').text('new cases today')).append($('<th>').text('new deaths today')).append($('<th>').text('recovered')))
+          
+         $('#thead tr').attr({"class":"p-3 mb-2 bg-dark text-white"})
+         $('#body tr').attr({"class":"table-active"})
+   
+   
+       for(i=1;i<=182;i++)
+       {
+         
+           tableb.append($('<tr>').append($('<td>').text(mydata.countryitems[0][i].title).attr({"class":"table-info"})).append($('<td>').text(mydata.countryitems[0][i].total_cases)).append($('<td>').text(mydata.countryitems[0][i].total_active_cases)).append($('<td>').text(mydata.countryitems[0][i].total_deaths)).append($('<td>').text(mydata.countryitems[0][i].total_new_cases_today).attr({"class":"table-danger"})).append($('<td>').text(mydata.countryitems[0][i].total_new_deaths_today)).append($('<td>').text(mydata.countryitems[0][i].total_recovered).attr({"class":"table-success"})))
+          
+   
+   
+       }
+          
         }
-    }
-    
-    $.ajax(settings).done(function (response) {
-       if(response.message!="OK"){console.log("country not found")}
-       else{
-        
-       tableh.children().remove()
-       tableb.children().remove()
-
-        tableh.attr({"class":"table-secondary"})
-        tableh.append($('<tr>').append($('<th>').text('city')).append($('<th>').text('confirmed')).append($('<th>').text('deaths')).append($('<th>').text('recovered')))
-        $('#lastupdate').text("Last-Update:  "+response.data.covid19Stats[0].lastUpdate).show()
-        
-
-
-    for(x of response.data.covid19Stats)
-    {
-      var city=x.keyId.split(','[0])
-        tableb.append($('<tr>').append($('<td>').text(city).attr({"class":"table-info"})).append($('<td>').text(x.confirmed).attr({"class":"warning"})).append($('<td>').text(x.deaths).attr({"class":"bg-danger"})).append($('<td>').text(x.recovered).attr({"class":"bg-success"})))
-       
-
-
-    }
- }
-        
-    });
+      });
     
 
 
-})
+
 
 
 
