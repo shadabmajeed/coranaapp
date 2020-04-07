cafded2149d87945d848b38d9b28aa0aa8397e20
shadabmajeed <44368474+shadabmajeed@users.noreply.github.com>
2020-04-07 13:49:57 +0530

    Update style.js

--- public/style.js
+++ public/style.js
@@ -3,7 +3,7 @@ var country=$('#country');
 var btn=$('#btn');
 var tableh=$('#thead')
 var tableb=$('#tbody')
-btn.click(()=>{})
+
     var conname=country.val()
     var val=conname.charAt(0).toUpperCase()+conname.substring(1).toLowerCase();
     var mydata;
@@ -16,21 +16,79 @@ btn.click(()=>{})
           tableb.children().remove()
    
            tableh.attr({"class":"table-secondary"})
-           tableh.append($('<tr>').append($('<th>').text('country')).append($('<th>').text('totalcases')).append($('<th>').text('active cases')).append($('<th>').text('deaths')).append($('<th>').text('new cases today')).append($('<th>').text('new deaths today')).append($('<th>').text('recovered')))
+           tableh.append($('<tr>').append($('<th>').html('country  &#8645;').attr({"class":"sort"})).append($('<th>').html('total-cases  &#8645;').attr({"class":"sort"})).append($('<th>').html('active-cases  &#8645;').attr({"class":"sort"})).append($('<th>').html('total-deaths  &#8645;').attr({"class":"sort"})).append($('<th>').html('cases-today  &#8645;').attr({"class":"sort"})).append($('<th>').html('deaths-today  &#8645;').attr({"class":"sort"})).append($('<th>').html('recovered  &#8645;').attr({"class":"sort"})))
           
          $('#thead tr').attr({"class":"p-3 mb-2 bg-dark text-white"})
-         $('#body tr').attr({"class":"table-active"})
-   
-   
-       for(i=1;i<=182;i++)
-       {
          
-           tableb.append($('<tr>').append($('<td>').text(mydata.countryitems[0][i].title).attr({"class":"table-info"})).append($('<td>').text(mydata.countryitems[0][i].total_cases)).append($('<td>').text(mydata.countryitems[0][i].total_active_cases)).append($('<td>').text(mydata.countryitems[0][i].total_deaths)).append($('<td>').text(mydata.countryitems[0][i].total_new_cases_today).attr({"class":"table-danger"})).append($('<td>').text(mydata.countryitems[0][i].total_new_deaths_today)).append($('<td>').text(mydata.countryitems[0][i].total_recovered).attr({"class":"table-success"})))
+         const arraydata=[];
+
+       
+        for(i=1;i<=182;i++)
+        {
+            arraydata.push(mydata.countryitems[0][i])
+            tableb.append($('<tr>').append($('<td>').text(mydata.countryitems[0][i].title).attr({"class":"table-info"})).append($('<td>').text(mydata.countryitems[0][i].total_cases)).append($('<td>').text(mydata.countryitems[0][i].total_active_cases)).append($('<td>').text(mydata.countryitems[0][i].total_deaths)).append($('<td>').text(mydata.countryitems[0][i].total_new_cases_today).attr({"class":"table-danger"})).append($('<td>').text(mydata.countryitems[0][i].total_new_deaths_today)).append($('<td>').text(mydata.countryitems[0][i].total_recovered).attr({"class":"table-success"})))
+            
+            
+    
+        }
+       
+       
+        
+   
+       $('.sort').click((event)=>{
+       
+       
+       y= event.target.textContent.split(' ')[0]
+       console.log(y)
+    if(y=="country"){}
+       else if(y=="total-cases"){
+           arraydata.sort((a,b)=>{
+              return b.total_cases-a.total_cases
+           })
+       }
+       else if(y=="active-cases"){
+        arraydata.sort((a,b)=>{
+            return b.total_active_cases-a.total_active_cases
+         }) 
+       }
+       else if(y=="total-deaths"){
+        arraydata.sort((a,b)=>{
+            return b.total_deaths-a.total_deaths
+         })
+       }
+       else if(y=="cases-today"){
+        arraydata.sort((a,b)=>{
+            return b.total_new_cases_today-a.total_new_cases_today
+         })
+       }
+       else if(y=="deaths-today"){
+         arraydata.sort((a,b)=>{
+            return b.total_new_deaths_today-a.total_new_deaths_today
+         })
+       }
+       else if(y=="recovered"){
+        arraydata.sort((a,b)=>{
+            return b.total_recovered-a.total_recovered
+         })
+       }   
+       
+       tableb.children().remove();
+      
+       for(i=1;i<=182;i++)
+       {   console.log(arraydata.length)
+        
+           tableb.append($('<tr>').append($('<td>').text(arraydata[i].title).attr({"class":"table-info"})).append($('<td>').text(arraydata[i].total_cases)).append($('<td>').text(arraydata[i].total_active_cases)).append($('<td>').text(arraydata[i].total_deaths)).append($('<td>').text(arraydata[i].total_new_cases_today).attr({"class":"table-danger"})).append($('<td>').text(arraydata[i].total_new_deaths_today)).append($('<td>').text(arraydata[i].total_recovered).attr({"class":"table-success"})))
           
    
    
        }
-          
+       
+    })
+
+
+
+
+
         }
       });
     
